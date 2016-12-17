9b457722c1f54a48c5395e8e39420de6e87769ba
qmxie <qmxie@thoughtworks.com>
2016-12-17 17:44:52 +0800

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,5 @@
+# Introduction to coding-note
+
+Practice clojure by implementing classic encipher method
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/coding_note/core_test.clj
@@ -0,0 +1,22 @@
+(ns coding-note.core-test
+  (:require [clojure.test :refer :all]
+            [coding-note.core :refer :all]))
+
+(deftest caesar-shift-test
+  (testing "Should CAESAR work"
+    (is (= (caesar-shift "xyz" 1) "yza"))
+    (is (= (caesar-shift "im glad we met" -1)
+           "hl fkzc vd lds"))))
+
+(deftest rail-fence-test
+  (testing "should RAIL-FENCE work"
+    (is (= (rail-fence "thelongestdaymusthaveanend" 2)
+           "teogsdyutaennhlnetamshvaed"))
+    (is (= (rail-fence "primedifferencebetweenelementsresmonsibleforhiroshimaandnagasaki" 8)
+           "pfeesesnretmmfhairweooigmeennrmaenetshasdcnsiiaaieerbrnkfblelodi"))))
+
+(deftest vigenere-test
+  (testing "should VIGENERE work"
+    (is (= (vigenere "tobeornottobethatisthequestion"
+                     "have")
+           "aowivrisatjfltceainxoelylsomvn"))))
